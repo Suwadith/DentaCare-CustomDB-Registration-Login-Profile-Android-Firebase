b5f802d035819c517285a685cddbb97c550b7663
Suwadith <srithar.2015214@iit.ac.lk>
2017-06-02 22:15:44 +0530

    Profile page update!

--- app/src/main/java/com/example/suwadith/dentacare_customdb_registration_android_firebase/UserProfile.java
+++ app/src/main/java/com/example/suwadith/dentacare_customdb_registration_android_firebase/UserProfile.java
@@ -1,6 +1,8 @@
 package com.example.suwadith.dentacare_customdb_registration_android_firebase;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,6 +12,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
@@ -21,10 +24,13 @@ import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class UserProfile extends AppCompatActivity implements View.OnClickListener {
 
     private FirebaseAuth firebaseAuth;
+    private FirebaseDatabase database;
+    private StorageReference storage;
 
     private ImageView userPic;
     private TextView userName;
@@ -33,15 +39,23 @@ public class UserProfile extends AppCompatActivity implements View.OnClickListen
     private TextView userDeviceID;
     private TextView userAddress;
     private Button logoutButton;
+    private Uri profileImgURL;
+    private static String emailID;
+    private ProgressDialog progressDialog;
+    private static String name;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
-
+        storage = FirebaseStorage.getInstance().getReference();
         //Initialization of the FirebaseAuth Object
         firebaseAuth = FirebaseAuth.getInstance().getInstance();
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        database = FirebaseDatabase.getInstance();
+
+        progressDialog = new ProgressDialog(this);
+
         DatabaseReference users = database.getReference("users");
 
         //Checking whether a user as already Logged In
@@ -50,6 +64,8 @@ public class UserProfile extends AppCompatActivity implements View.OnClickListen
             //Starting the User Login Activity if the user is not Logged in
             startActivity(new Intent(this, Login.class));
         }
+
+
 
         final FirebaseUser user = firebaseAuth.getCurrentUser();
 
@@ -66,8 +82,13 @@ public class UserProfile extends AppCompatActivity implements View.OnClickListen
 
         //Setting the userEmail field text to show the logged in user's email ID
         userEmail.setText(user.getEmail());
+        emailID = user.getEmail();
 
-        final String email = user.getEmail();
+        /*try{
+            userPic.setImageIcon(storage.child("Photos").child(emailID).child("myImage.jpg"));
+        }catch(){
+
+        }*/
 
         final Query userQuery = users.orderByChild("Email");
 
@@ -75,17 +96,15 @@ public class UserProfile extends AppCompatActivity implements View.OnClickListen
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 for(DataSnapshot post : dataSnapshot.getChildren() ){
-                    if(post.child("Email").getValue().equals(email)){
+                    if(post.child("Email").getValue().equals(user.getEmail())){
                         Log.d("Output", "Found");
-                        final String username = post.getKey().toString();
-                        Log.d("Output", username);
-                        userName.setText(username);
-                        final String deviceID = post.child("DeviceID").getValue().toString();
-                        userDeviceID.setText(deviceID);
-                        final String address = post.child("Address").getValue().toString();
-                        userAddress.setText(address);
+                        Log.d("Output", post.getKey().toString());
+                        userName.setText(post.getKey().toString());
+                        name=post.getKey().toString();
+                        userDeviceID.setText(post.child("DeviceID").getValue().toString());
+                        userAddress.setText(post.child("Address").getValue().toString());
                     }else{
-                       Log.d("Output", "Failure");
+                        Log.d("Output", "Failure");
                     }
 
                     Log.d("Output", post.child("Email").toString());
@@ -116,21 +135,54 @@ public class UserProfile extends AppCompatActivity implements View.OnClickListen
             //Switches to login Activity
             startActivity(new Intent(this, Login.class));
         }else if(view == changeImageButton){
-                // file picker
-                Intent i = new Intent();
-                i.setType("image/*");
-                i.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(Intent.createChooser(i, "Select Picture"), 100);
+            // file picker
+            Intent i = new Intent();
+            i.setType("image/*");
+            i.setAction(Intent.ACTION_GET_CONTENT);
+            startActivityForResult(Intent.createChooser(i, "Select Picture"), 100);
 
         }
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 100 && resultCode == RESULT_OK){
+
+            progressDialog.setMessage("Uploading ..... ");
+            progressDialog.show();
+
+            profileImgURL = data.getData();
+
+            /*StorageReference imagePath = storage.child("Photos").child(emailID).child(profileImgURL.getLastPathSegment());*/
+
+            StorageReference imagePath = storage.child("Photos").child(emailID).child("myImage.jpg");
+
+            DatabaseReference users = database.getReference("users");
+            users.child(name).child("Username").setValue(imagePath);
+
+            Log.d("Output", imagePath.toString());
+
+            imagePath.putFile(profileImgURL).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                    Toast.makeText(UserProfile.this, "Upload Finished", Toast.LENGTH_LONG).show();
+
+                    progressDialog.dismiss();
+
+                }
+            });
+
+        }
+    }
 
     private void changeImage(){
 
-        FirebaseStorage imageStorage = FirebaseStorage.getInstance();
-        StorageReference storageRef = imageStorage.getReferenceFromUrl("gs://dentacare-47bea.appspot.com");
-
+        /*FirebaseStorage imageStorage = FirebaseStorage.getInstance();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        StorageReference storageRef = imageStorage.getReferenceFromUrl("gs://dentacare-47bea.appspot.com");*/
 
     }
 }
